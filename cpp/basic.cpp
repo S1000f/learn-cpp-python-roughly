8fd25d32c592e331a7d2a6bd23de7c73ccfda217
dohpkim <dohp.kim@gmail.com>
2020-07-15 00:32:37 +0900

    cpp

--- cpp/basic.cpp
+++ cpp/basic.cpp
@@ -5,5 +5,20 @@ int main() {
     std::cout << "Hello world";
     std::cout << std::endl;
     
+    char a = 'b';
+    std::cout << "a" << std::endl;
+    std::cout << a << std::endl;
+    std::cout << "a: " << a << std::endl;
+
+    int b = 5;
+    std::cout << "b = " << b;
+    std::cout << std::endl;
+
+    std::cout << (int)a << std::endl;
+    
+
+
+
+
     return 0;
 }
