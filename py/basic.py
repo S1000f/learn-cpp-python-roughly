4ef8fc894683fb56d6647b51986bd226aedcb345
dohpkim <dohp.kim@gmail.com>
2020-07-18 20:42:50 +0900

    python

---
+++ py/basic.py
@@ -0,0 +1,18 @@
+print('Hello world!')
+
+a = 'A'
+print("a")
+print(a)
+print("a:", a)
+
+b = 5
+print("b:", b)
+print(ord(a), b)
+
+a = 'B'
+print(chr(ord(a) - 1), a, chr(ord(a) + 1))
+
+a = 11
+print(hex(a))
+print(a)
+print(oct(a))
---
+++ py/operator.py
@@ -0,0 +1,35 @@
+a = 7
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(int(a/b))
+print(a % b)
+
+a = 10
+print(a << 2)
+print(a >> 2)
+
+b = 5
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+a = False
+b = True
+print(a and b)
+print(a or b)
+
+a = 10
+b = 9
+print(a & b)
+print(a | b)
+
+a = 10
+a += 4
+print(a)
+
+a /= 2
+print(int(a))
